ea5f4ffbecb22e10ad9c46336c85d3e1f2a3ba3b
Nate Schutta <ntschutta@gmail.com>
2013-11-11 21:38:57 -0600

    added initial grunt config

--- app/Gruntfile.js
+++ app/Gruntfile.js
@@ -1,3 +1,11 @@
 module.exports = function(grunt) {
-  // Do grunt-related things in here
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+      build: {
+        src: 'src/<%= pkg.name %>.js',
+        dest: 'build/<%= pkg.name %>.min.js'
+      }
+    }
+  });
 };
