6421e4b223e5cfd841175a087c5a378f520b0c29
Nate Schutta <ntschutta@gmail.com>
2013-11-13 18:26:30 -0600

    added various snippet files

--- code-snippets/todo.js
+++ code-snippets/todo.js
@@ -3,3 +3,24 @@ describe("A suite", function() {
     expect(true).toBe(true);
   });
 });
+
+describe("A suite is just a function", function() {
+  var a;
+
+  it("and so is a spec", function() {
+    a = true;
+
+    expect(a).toBe(true);
+  });
+});
+
+describe("Included matchers:", function() {
+
+  it("The 'toBe' matcher compares with ===", function() {
+    var a = 12;
+    var b = a;
+
+    expect(a).toBe(b);
+    expect(a).not.toBe(null);
+  });
+});
