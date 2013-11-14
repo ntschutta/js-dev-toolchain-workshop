c1873c2d992134c3dfa0563e8e8acfb91f0e8b94
Nate Schutta <ntschutta@gmail.com>
2013-11-13 23:01:53 -0600

    added configuration to run jasmine tests via grunt

--- app/Gruntfile.js
+++ app/Gruntfile.js
@@ -1,7 +1,22 @@
 module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
+    pkg: grunt.file.readJSON('package.json'),
+	jasmine: {
+		customTemplate: {
+			src: 'src/**/*.js',
+			options: {
+				specs: 'test/spec/*Spec.js',
+				vendor: [
+				   'lib/jquery.js',
+			       'lib/underscore.js',
+			       'lib/backbone.js',
+			       'lib/backbone.localStorage.js'
+			    ]
+			}
+		}
+	}
   });
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
 
 };
